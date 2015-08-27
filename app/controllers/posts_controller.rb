b0728f693b9b1b5c479e1b507c8998228ea3ef96
jane3wallace <jane3wallace@gmail.com>
2015-08-26 20:50:59 -0400

    first commit

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,4 +8,30 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
   end
 
+  def new
+    @post = Post.new
+  end
+
+  def create
+    post = Post.new
+    post.title = params[:post][:title]
+    post.body = params[:post][:body]
+    if post.save
+      flash[:notice] = "Your post was created!"
+    else
+      flash[:alert] = "Sorry, this post was not created."
+    end
+    redirect_to post_path(post.id)
+  end
+
+  def destroy
+    post = Post.find(params[:id])
+    if post.destroy
+      flash[:notice] = "The post was deleted."
+    else
+      flash[:alert] = "This post cannot be deleted."
+    end
+    redirect_to posts_path
+  end
+
 end
